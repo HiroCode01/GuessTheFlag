0cac835a3c4285fe14b6f06939a7d795bda006e1
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-04-01 22:06:45 +0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Fazliddin Abdazimov on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
